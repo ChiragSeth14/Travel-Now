c4b79bdf98a9dc94e5ac8854b377e27818c8f878
ChiragSeth14 <sethchirag1412@gmail.com>
2023-11-04 17:41:31 -0700

    Initial Commit

---
+++ Sunhacks 2023/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sunhacks 2023
+//
+//  Created by Chirag Seth on 11/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sunhacks 2023/Sunhacks_2023App.swift
@@ -0,0 +1,17 @@
+//
+//  Sunhacks_2023App.swift
+//  Sunhacks 2023
+//
+//  Created by Chirag Seth on 11/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Sunhacks_2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
